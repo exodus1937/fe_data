432fe2e06f8f930f51a0ce67ebd3eb53102fc767
dulei <duleigiser@icloud.com>
2016-05-26 15:15:06 +0800

    Create kaohe.js

---
+++ kaohe.js
@@ -0,0 +1,47 @@
+//首页考核
+
+//二级页面总揽
+//url:http://127.0.0.1:8080/jsjd/getKaoheAction.do?method=getTongJi&orgid=''; 
+//字段描述：  
+
+//考核二级页面detail
+//Url:http://127.0.0.1:8080/jsjd/getKaoheAction.do?method=getKaoheShow&org_id=”” 
+//参数：orgid
+//字段描述：
+
+{
+  "monthnum":"157",    //月度考核次数
+  "yearnum":"157",     //本年考核数量
+  "monthAmount":"9950",//本月考核金额
+  "yearAmount":"9950"  //本年考核金额
+}
+
+
+
+//参数：org_id
+//字段描述：
+/*        amount    金额
+          orgId 电厂ID
+          id  主ID
+          infomation 通知显示内容
+          status 流程状态（申述状态）流程状态(1：草稿2：审核中3：审核通过4：驳回)
+          tag 是否已申诉标记
+          date 考核日期
+          orgName 电厂名称	
+          key 是否考核
+          mode 考核方式
+          */
+          //数据
+          {
+            "amount":"50",
+            "orgId":"c21834b4-1cb0-490f-a2a8-deeaf7f7e065",
+            "id":"f2a10e15-4f00-4802-8cde-3a2040137055",
+            "infomation":"2016年第1季度岱海发电节能专业未填写上季度总结考核",
+            "status":"null",
+            "tag":"N",
+            "date":"2016-05-24",
+            "orgName":"岱海发电",
+            "key":"是",
+            "mode":"自动考核"
+          }
+         
